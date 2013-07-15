6fd3cfdb2b5eaf78c7b2b5b1b0d5a3df9224c56a
Luka Mirosevic <luka@goonbee.com>
2013-07-15 16:03:05 +0200

    Initial Commit

---
+++ GBAdvancedTableViewController/GBAdvancedTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  GBAdvancedTableViewController.h
+//  GBAdvancedTableViewController
+//
+//  Created by Luka Mirosevic on 15/07/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GBAdvancedTableViewController : NSObject
+
+@end
---
+++ GBAdvancedTableViewController/GBAdvancedTableViewController.m
@@ -0,0 +1,13 @@
+//
+//  GBAdvancedTableViewController.m
+//  GBAdvancedTableViewController
+//
+//  Created by Luka Mirosevic on 15/07/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import "GBAdvancedTableViewController.h"
+
+@implementation GBAdvancedTableViewController
+
+@end
