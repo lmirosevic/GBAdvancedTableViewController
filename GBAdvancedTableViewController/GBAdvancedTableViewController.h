e808ff0eadb4744abf2c814501358493a13bd10b
Luka Mirosevic <luka@goonbee.com>
2013-08-08 14:14:08 +0200

    looking good

--- GBAdvancedTableViewController/GBAdvancedTableViewController.h
+++ GBAdvancedTableViewController/GBAdvancedTableViewController.h
@@ -8,6 +8,19 @@
 
 #import <Foundation/Foundation.h>
 
-@interface GBAdvancedTableViewController : NSObject
+#import "GBToolbox.h"
+#import "ODRefreshControl.h"
+
+@interface GBAdvancedTableViewController : GBTableViewController
+
+@property (assign, nonatomic) BOOL                                          shouldShowRefreshControl;               //defaults to NO
+@property (assign, nonatomic) BOOL                                          shouldAutomaticallyFinishRefreshing;    //defaults to NO
+
+-(void)refresh;
+-(void)didFinishRefreshing;
+
+//you can add additional targets if you like
+-(void)addTargetForRefreshAction:(id)target withAction:(SEL)selector;
+-(void)removeTargetForRefreshControlAction:(id)target withAction:(SEL)selector;
 
 @end
--- GBAdvancedTableViewController/GBAdvancedTableViewController.m
+++ GBAdvancedTableViewController/GBAdvancedTableViewController.m
@@ -8,6 +8,70 @@
 
 #import "GBAdvancedTableViewController.h"
 
+static BOOL kDefaultShouldShowRefreshControl =                  NO;
+static BOOL kDefaultShouldAutomaticallyFinishRefreshing =       NO;
+
+@interface GBAdvancedTableViewController ()
+
+@property (strong, nonatomic) ODRefreshControl                  *refreshControl;
+
+@end
+
 @implementation GBAdvancedTableViewController
+
+#pragma mark - CA
+
+-(void)setShouldShowRefreshControl:(BOOL)shouldShowRefreshControl {
+    _shouldShowRefreshControl = shouldShowRefreshControl;
+    
+    if (shouldShowRefreshControl) {
+        self.refreshControl = [[ODRefreshControl alloc] initInScrollView:self.tableView];
+        [self.refreshControl addTarget:self action:@selector(refreshAction:) forControlEvents:UIControlEventValueChanged];
+    }
+    else {
+        self.refreshControl = nil;
+    }
+}
+
+#pragma mark - overrides
+
+-(id)initWithStyle:(UITableViewStyle)style {
+    if (self = [super initWithStyle:style]) {
+        self.shouldShowRefreshControl = kDefaultShouldShowRefreshControl;
+        self.shouldAutomaticallyFinishRefreshing = kDefaultShouldAutomaticallyFinishRefreshing;
+    }
+    
+    return self;
+}
+
+#pragma mark - actions
+
+-(void)refreshAction:(id)sender {
+    if (self.shouldAutomaticallyFinishRefreshing) {
+        [self.refreshControl endRefreshing];
+
+        //moves the table back up
+        self.tableView.scrollEnabled = NO;
+        self.tableView.scrollEnabled = YES;
+    }
+    
+    [self refresh];
+}
+
+#pragma mark - API
+
+-(void)refresh _abstract
+
+-(void)didFinishRefreshing {
+    [self.refreshControl endRefreshing];
+}
+
+-(void)addTargetForRefreshAction:(id)target withAction:(SEL)selector {
+    [self.refreshControl addTarget:target action:selector forControlEvents:UIControlEventValueChanged];
+}
+
+-(void)removeTargetForRefreshControlAction:(id)target withAction:(SEL)selector {
+    [self.refreshControl removeTarget:target action:selector forControlEvents:UIControlEventValueChanged];
+}
 
 @end
